25077e00c17079b262336cd3c13f3d734c6cb13d
Julian <liannsd3@gmail.com>
2023-07-04 10:58:36 -0300

    routing works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -105,10 +105,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col text-slate-300">
         <div className="flex gap-1">
-          <span>{`@${author.username}`}</span>
-          <span className="font-thin">
-            {` · ${dayjs(post.createdAt).fromNow()}`}{" "}
-          </span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">
+              {` · ${dayjs(post.createdAt).fromNow()}`}{" "}
+            </span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
