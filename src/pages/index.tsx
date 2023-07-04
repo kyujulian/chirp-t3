882ddee9224525f30dd4f12e2878de0a39306e4b
Julian <liannsd3@gmail.com>
2023-07-03 23:31:21 -0300

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,7 @@ const PostView = (props: PostWithUser) => {
             {` · ${dayjs(post.createdAt).fromNow()}`}{" "}
           </span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
